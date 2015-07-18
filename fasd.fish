297d5c87daf456b4581e403627e94175a23f58ae
Tom Hensel <github@jitter.eu>
2015-07-18 19:21:10 +0200

    Revert "adopt to 'available' lib changes"
    This reverts commit 8f81a5026a02831fca731f21d5e9e7c1a43a6af5.

--- fasd.fish
+++ fasd.fish
@@ -1,8 +1,9 @@
 # extracted from https://jurriaan.ninja/2014/12/21/fish-shell-and-fasd.html
 
 function init --on-event init_fasd
-  if not exec_avail fasd
-    echo "🍒  Please install 'fasd' first!"
+  set -l fasd_bin (which fasd)
+  if not test -n "$fasd_bin"
+    echo "🍒  Please install 'fasd' first: brew install fasd"
   else
     function -e fish_preexec _run_fasd
       fasd --proc (fasd --sanitize "$argv") > "/dev/null" 2>&1
