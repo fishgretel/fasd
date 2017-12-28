9388a6a7f6e683ad06002f740c44781856c03d6d
Aditya Mukherjee <aditya@adityamukherjee.com>
2017-12-28 12:44:42 +0530

    fix fasd not recognising relative filenames

--- conf.d/__fasd_run.fish
+++ conf.d/__fasd_run.fish
@@ -1,4 +1,3 @@
   function __fasd_run -e fish_preexec -d "fasd takes record of the directories changed into"
-    command fasd --proc (command fasd --sanitize "$argv") > "/dev/null" 2>&1 &
+    command fasd --proc (command fasd --sanitize (eval echo "$argv") | tr -s ' ' \n) > "/dev/null" 2>&1 &
   end
-  
