16887a5e2ffcc5df33680651f44d795f1f3d6759
Aditya Mukherjee <aditya@adityamukherjee.com>
2017-12-29 22:24:46 +0530

    fix indents

--- completions/fasd.fish
+++ completions/fasd.fish
@@ -2,9 +2,9 @@ complete -c fasd -f
 complete -c fasd -s s -d "List all paths and scores" -f
 complete -c fasd -s l -d "List paths without scores" -f
 complete -c fasd -s i -d "Interactive mode" -f
-complete -c fasd -s e -a "subl atom" -d "Set command to execute on the result file" -f
-complete -c fasd -s b -a "spotlight viminfo recently-used current" -d "Only use given backend" -f
-complete -c fasd -s B -a "spotlight viminfo recently-used current" -d "Add additional backend" -f
+complete -c fasd -s e -a (echo subl atom) -d "Set command to execute on the result file" -f
+complete -c fasd -s b -a (echo spotlight viminfo recently-used current) -d "Only use given backend" -f
+complete -c fasd -s B -a (echo spotlight viminfo recently-used current) -d "Add additional backend" -f
 complete -c fasd -s a -d "Match files and directories" -f
 complete -c fasd -s d -d "Match directories only" -f
 complete -c fasd -s f -d "Match files only" -f
